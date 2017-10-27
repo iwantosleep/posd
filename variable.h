e87bf8707debe75ef179cd87da5b4b09ba9f34fb
wwwXOX <trap30216@gmail.com>
2017-10-27 15:51:58 +0800

    hw4

--- variable.h
+++ variable.h
@@ -2,21 +2,29 @@
 #define VARIABLE_H
 
 #include <string>
-#include "term.h"
-#include <vector>
-//#include "struct.h"
-//using std::string;
-using namespace std;
+#include "atom.h"
+using std::string;
 
-class Variable : public Term{
-	public:
-		bool match ( Term &term );
-  		Variable (string s);
-		string value () const;
-		vector <Variable *> _Varlis;//
-	private:
-		string _value; 
-		bool _Varflag = true;
-		Term * _Vart={};
+class Variable : public Term {
+public:
+  Variable(string s):Term(s), _inst(0){}
+  string value() const {
+    if (_inst)
+      return _inst->value();
+    else
+      return Term::value();
+  }
+  bool match( Term & term ){
+    if (this == &term)
+      return true;
+    if(!_inst){
+      _inst = &term ;
+      return true;
+    }
+    return _inst->match(term);
+  }
+private:
+  Term * _inst;
 };
+
 #endif
